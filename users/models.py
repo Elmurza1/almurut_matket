62bb975c90638fb86590879d3163ae02209c1e90
elmyrza1 <elmyrza217@gmail.com>
2024-09-02 19:41:10 +0600

    user

---
+++ users/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+    """пользователь"""
+    first_name = models.CharField(max_length=111)
+
+    last_name = models.CharField(max_length=111)
+
+    email = models.EmailField(unique=True)
+    password = models.TextField()
+
+    phone_number = models.CharField(
+        max_length=22,
+        null=True,
+        blank=True
+    )
+    address = models.TextField()
+    avatar = models.ImageField()
+    birthdate = models.DateField()
+
+    date_join = models.DateTimeField(auto_now_add=True)
+    is_superuser = models.BooleanField()
+    is_staff = models.BooleanField()
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        unique_together = ('first_name', 'last_name',)
+
+
+
