3b325f1628721fa8c382375feef7f78c49844452
elmyrza1 <elmyrza217@gmail.com>
2024-10-16 19:30:20 +0600

    test

--- market/models.py
+++ market/models.py
@@ -39,7 +39,7 @@ class Product(models.Model):
 
     def get_price_with_sales(self):
         """Возвращает цену с учётом скидки"""
-        if self.price is None or self.sales_percent is None:
+        if self.price or self.sales_percent is None:
             return self.price  # Можешь вернуть 0 или другое значение, если одно из полей пустое
 
         if self.sales_percent == 0:
--- market/tests.py
+++ market/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.utils.datetime_safe import datetime
+from .models import Category, Product
 
-# Create your tests here.
+
+class ProductListTestCase(TestCase):
+    def test_product_list_success(self):
+        response = self.client.get('/product-list/')
+        self.assertEqual(response.status_code, 200)
+
+
+
+class ProductDetailTestCase(TestCase):
+    def test_product_detail_success(self):
+        some_date = datetime(year=2024, month=3, day=4)
+        category = Category.objects.create(name='phone')
+
+        product = Product.objects.create(
+            category=category,
+            name='asus',
+            price=50000,
+            sales_procent=None,
+            description='goood',
+            new_expiry_date=some_date,
+            preview_image='media/tovat/file-jpeg',
+            is_new=True
+        )
+
+        response = self.client.get(f'/product-list/{product.id}/')
+
+        self.assertEqual(response.status_code, 200)
+
+
+
+
+
+
